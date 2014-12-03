cc61389506d2ecfdf0277bfd673b812da5dd04a7
Lewis <lmenelaws112@gmail.com>
2014-12-02 22:51:45 -0500

    Fixed Home bug

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -5,7 +5,7 @@
  </head>
  <body>
    <h1>Home</h1>
-   <h2>Welcome <?php echo $firstname; echo $lastname ?>!</h2>
+   <h2>Welcome <?php echo $username ?>!</h2>
    <a href="home/logout">Logout</a>
  </body>
 </html>
